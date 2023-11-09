2608cd5a9b118840eb468941c851fd440a277d3c
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2023-11-09 10:11:24 +0700

    Create constant for common data

--- dress-shop/src/App.tsx
+++ dress-shop/src/App.tsx
@@ -12,6 +12,7 @@ import Header from './components/Header'
 import GlobalStyles from './globalStyles'
 import Footer from './components/Footer'
 import Card from './components/Card'
+import { CARD } from './constants/common'
 
 const router = createBrowserRouter([
   {
@@ -24,15 +25,6 @@ const router = createBrowserRouter([
   }
 ])
 
-// Data Mock - will remove when have a real data
-const data = {
-  isNew: true,
-  imageURL:
-    'https://dress-shop.vercel.app/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdjlbfjouc%2Fimage%2Fupload%2Fv1581158167%2Fsbiuoziiqi5gkuvrsymv.jpg&w=1080&q=75',
-  name: 'Long Sleeves Polka Dots',
-  price: 'P800',
-}
-
 function App() {
   return (
     <ChakraProvider theme={CHAKRA_THEME_DEFAULT}>
@@ -42,10 +34,10 @@ function App() {
       <Footer />
       <GlobalStyles />
       <Card
-        src={data.imageURL}
-        altText={data.name}
-        name={data.name}
-        price={data.price}
+        src={CARD.imageURL}
+        altText={CARD.name}
+        name={CARD.name}
+        price={CARD.price}
       />
     </ChakraProvider>
   )
---
+++ dress-shop/src/constants/common.ts
@@ -0,0 +1,6 @@
+export const CARD = {
+  imageURL:
+    'https://dress-shop.vercel.app/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdjlbfjouc%2Fimage%2Fupload%2Fv1581158167%2Fsbiuoziiqi5gkuvrsymv.jpg&w=1080&q=75',
+  name: 'Long Sleeves Polka Dots',
+  price: 800,
+}
