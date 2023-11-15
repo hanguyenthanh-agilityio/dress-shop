0d9336f0ba69e284274c4396beeacb5dac79d966
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2023-11-15 14:01:10 +0700

    Update unit test

--- dress-shop/src/components/Cart/CartHeader/CartHeader.test.tsx
+++ dress-shop/src/components/Cart/CartHeader/CartHeader.test.tsx
@@ -5,13 +5,14 @@ import CardHeader from '.';
 
 // Constants
 import { HEADER_LIST } from '../../../constants/cart';
-
-jest.mock("./index");
+import { Table } from '@chakra-ui/react';
 
 describe('CardHeader component', () => {
   it('Should render CardHeader snapshot correctly', () => {
     const cardHeader = render(
-      <CardHeader headerList={HEADER_LIST} />
+      <Table>
+        <CardHeader headerList={HEADER_LIST} />
+      </Table>
     );
 
     expect(cardHeader).toMatchSnapshot();
