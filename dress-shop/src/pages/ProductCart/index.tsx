a0ca9a19aef9f5d4493ad4fd51981954f73d990d
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2023-11-17 19:23:49 +0700

    Update responsive for header

--- dress-shop/src/pages/ProductCart/index.tsx
+++ dress-shop/src/pages/ProductCart/index.tsx
@@ -18,16 +18,21 @@ const ProductCart = () => {
         textTransform="uppercase"
         pt="35px"
         fontWeight="700"
-        size={{xs: "medium", lg: "default"}}
+        size={{ xs: "medium", lg: "default" }}
       >
         Your Cart
       </Heading>
       <Cart headerList={HEADER_LIST} products={PRODUCT_CART} total={0} />
       <Flex flexDir="column" alignItems="end">
-        <Flex  my="30px">
-          <Text size={{xs: "small", lg: "large"}} pr="50px">Sub Total</Text>
+        <Flex my="30px" alignItems="center">
           <Text
-            size={{xs: "large", lg: "wide"}}
+            size={{ xs: "small", lg: "large" }}
+            pr={{ xs: "30px", lg: "50px" }}
+          >
+            Sub Total
+          </Text>
+          <Text
+            size={{ xs: "large", lg: "wide" }}
             color="text.primary"
             fontWeight="600"
           >
@@ -37,17 +42,15 @@ const ProductCart = () => {
         <Flex>
           <Button
             variant="check"
-            p={{xs:"20px", lg:"25px"}}
-            size={{xs:"small",
-            lg:"default"}}
+            p={{ xs: "20px", lg: "25px" }}
+            size={{ xs: "small", lg: "default" }}
           >
             Check out
           </Button>
         </Flex>
       </Flex>
     </Container>
-  )
-}
-
+  );
+};
 
 export default ProductCart;
