e98453b598e6b487aa033e895f48d2d587278980
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2023-11-17 18:38:46 +0700

    Update style for footer

--- dress-shop/src/components/Footer/index.tsx
+++ dress-shop/src/components/Footer/index.tsx
@@ -1,51 +1,102 @@
-import { Box, Container, Flex, Stack, Text } from '@chakra-ui/react';
+import { useCallback } from "react";
+import { Box, Container, Flex, Stack, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
+// Hooks
+import { useBreakPoints } from "../../hooks/useBreakPoints";
 
 // React icons
-import { FaFacebook, FaInstagram, FaGithub, FaHome, FaSearch } from 'react-icons/fa'
+import {
+  FaFacebook,
+  FaInstagram,
+  FaGithub,
+  FaHome,
+  FaSearch,
+  FaCartArrowDown,
+} from "react-icons/fa";
 import { IconContext } from "react-icons";
-import { useBreakPoints } from '../../hooks/useBreakPoints';
 
 const Footer = () => {
-  const { isLargeThanTablet } = useBreakPoints()
+  const navigate = useNavigate();
+  const { isLargeThanTablet } = useBreakPoints();
 
-  const handleClickHome = () => {}
-  const handleClickSearch = () => {}
+  const handleClickHome = useCallback(() => {
+    navigate("/");
+  }, [navigate]);
+
+  const handleClickSearch = useCallback(() => {
+    navigate("/search");
+  }, [navigate]);
+
+  const handleClickCart = useCallback(() => {
+    navigate("/cart");
+  }, [navigate]);
 
   const actionButton = [
-    {id: "men-categories", icon: <FaHome />, text: "Home", action: handleClickHome},
-    {id: "women-categories", icon: <FaSearch />, text: "Search", action: handleClickSearch},
-  ]
+    {
+      id: "men-categories",
+      icon: <FaHome />,
+      text: "Home",
+      action: handleClickHome,
+    },
+    {
+      id: "women-categories",
+      icon: <FaSearch />,
+      text: "Search",
+      action: handleClickSearch,
+    },
+    {
+      id: "men-categories",
+      icon: <FaCartArrowDown />,
+      text: "Cart",
+      action: handleClickCart,
+    },
+  ];
   return (
     <>
       {isLargeThanTablet ? (
-        <Box
-          bg="background.dark"
-          color="text.default">
-        <Container>
-          <Flex justifyContent="space-between" alignItems="center" py="15px">
-            <Text
-              color="white"
-              my="15px"
-              size={{xs: "tiny", lg: "default"}}
-            >
-              © 2020 Dress Shop. All rights reserved.
-            </Text>
-            <Stack direction="row" spacing={6}>
-              <IconContext.Provider value={{ size:"22px"}}>
-                <FaFacebook />
-                <FaInstagram />
-                <FaGithub />
-              </IconContext.Provider>
-            </Stack>
-          </Flex>
-        </Container>
-      </Box>
+        <Box bg="background.dark" color="text.default">
+          <Container>
+            <Flex justifyContent="space-between" alignItems="center" py="15px">
+              <Text
+                color="white"
+                my="15px"
+                size={{ xs: "tiny", lg: "default" }}
+              >
+                © 2020 Dress Shop. All rights reserved.
+              </Text>
+              <Stack direction="row" spacing={6}>
+                <IconContext.Provider value={{ size: "22px" }}>
+                  <FaFacebook />
+                  <FaInstagram />
+                  <FaGithub />
+                </IconContext.Provider>
+              </Stack>
+            </Flex>
+          </Container>
+        </Box>
       ) : (
-        <Flex borderTop="1px solid #ccc" overflow="hidden" pos="fixed" bottom="0" w="100%">
-          {actionButton.map((item)=> (
+        <Flex
+          justifyContent="space-around"
+          bg="#fff"
+          borderTop="1px solid #ccc"
+          overflow="hidden"
+          pos="fixed"
+          bottom="0"
+          w="100%"
+          p="10px"
+        >
+          {actionButton.map((item) => (
             <Stack direction="row" spacing={6}>
-              <Flex>
-                {item.icon}
+              <Flex
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                onClick={item.action}
+              >
+                <IconContext.Provider value={{ size: "20px" }}>
+                  {item.icon}
+                </IconContext.Provider>
                 <Text>{item.text}</Text>
               </Flex>
             </Stack>
@@ -53,7 +104,6 @@ const Footer = () => {
         </Flex>
       )}
     </>
-
   );
 };
 
