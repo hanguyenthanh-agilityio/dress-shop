f33eeefa772946ae4c02498d0096bdd590c34cd6
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2023-11-27 08:09:32 +0700

    Update code style

--- dress-shop/src/layouts/MainLayout/index.tsx
+++ dress-shop/src/layouts/MainLayout/index.tsx
@@ -1,8 +1,8 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
 // Components
-import Footer from '../../components/Footer';
-import HeaderContainer from '../HeaderLayout';
+import Footer from "../../components/Footer";
+import HeaderContainer from "../HeaderContainer";
 
 const MainLayout = (): JSX.Element => (
   <>
