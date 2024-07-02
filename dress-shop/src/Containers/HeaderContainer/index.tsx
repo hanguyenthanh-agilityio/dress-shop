ea3708f1e8cb3c14c8895493832c1c6cdcec7493
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-07-02 15:10:04 +0700

    Handle search with enter key

--- dress-shop/src/Containers/HeaderContainer/index.tsx
+++ dress-shop/src/Containers/HeaderContainer/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useCallback, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 // Components
@@ -19,11 +19,9 @@ const HeaderContainer = () => {
     setSearchValue(e.target.value);
   };
 
-  // Handle search product
-  const handleSearch = useCallback(
-    (e: React.MouseEvent) => {
+  const handleKeyDown = (e: { key: string }) => {
+    if (e.key == "Enter") {
       navigate(ROUTES.PRODUCT_SEARCH);
-      e.preventDefault();
 
       if (searchValue.length === 0) {
         searchParams.delete("search");
@@ -36,14 +34,13 @@ const HeaderContainer = () => {
           replace: true,
         });
       }
-    },
-    [navigate, searchParams, searchValue, setSearchParams],
-  );
+    }
+  };
 
   return (
     <Header
       onChange={handleChange}
-      onSubmit={handleSearch}
+      onKeyDown={handleKeyDown}
       value={searchValue}
     />
   );
