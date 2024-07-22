6a519dfd2a0a1ac1c17df76a2f0a027f7ab65455
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-07-22 15:23:54 +0700

    fix: update apply errorboundary

--- dress-shop/src/App.tsx
+++ dress-shop/src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Themes
 import CHAKRA_THEME_DEFAULT from "./themes/chakra";
@@ -9,7 +10,9 @@ import { router } from "./routes";
 
 // Stores
 import Context from "./stores/Context";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Components
+import { ErrorBoundary } from "./components";
 
 function App() {
   const queryClient = new QueryClient({
@@ -23,7 +26,9 @@ function App() {
       <ChakraProvider theme={CHAKRA_THEME_DEFAULT}>
         <Context>
           <CSSReset />
-          <RouterProvider router={router} />
+          <ErrorBoundary>
+            <RouterProvider router={router} />
+          </ErrorBoundary>
         </Context>
       </ChakraProvider>
     </QueryClientProvider>
