31df5b6f6d2142ecc61956c8350523f7097e44f2
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-07-23 15:08:49 +0700

    fix: implement HeaderProduct component

---
+++ dress-shop/src/components/SearchProduct/index.tsx
@@ -0,0 +1,50 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { ChangeEvent, useCallback, useState } from "react";
+
+// Components
+import SearchInput from "../SearchInput";
+
+// Constants
+import { ROUTES } from "@/constants";
+
+const SearchProduct = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchValue, setSearchValue] = useState(
+    searchParams.get("search") || "",
+  );
+
+  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  }, []);
+
+  const handleKeyDown = useCallback(
+    (e: { key: string }) => {
+      if (e.key == "Enter") {
+        navigate(ROUTES.PRODUCT_SEARCH);
+
+        if (searchValue.length === 0) {
+          searchParams.delete("search");
+        } else {
+          searchParams.set("search", searchValue);
+        }
+
+        searchParams.delete("category");
+
+        setSearchParams(searchParams, {
+          replace: true,
+        });
+      }
+    },
+    [navigate, searchParams, searchValue, setSearchParams],
+  );
+  return (
+    <SearchInput
+      value={searchValue}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+    />
+  );
+};
+
+export default SearchProduct;
