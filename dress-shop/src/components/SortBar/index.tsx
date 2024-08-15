1aeed4c4cdc764eb79f1d33146e146d485db4c02
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-08-15 15:22:36 +0700

    fix: remove handle SortBar in ProductSearch page

--- dress-shop/src/components/SortBar/index.tsx
+++ dress-shop/src/components/SortBar/index.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, lazy, memo, Suspense, useCallback } from "react";
-import { Button, Flex, Text, useDisclosure } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
+import { Button, Flex, Text, useDisclosure, useToast } from "@chakra-ui/react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 // Components
 import { LoadingIndicator, Select } from "@/components";
@@ -11,130 +11,167 @@ import { Category, Product } from "@/types";
 
 // Constants
 import { MEN_CATEGORY, OPTION_SORT, ROUTES, WOMEN_CATEGORY } from "@/constants";
+import { useAddProduct } from "@/hooks";
+import { AxiosError } from "axios";
 
-interface SortBarProps {
-  onChangeSelect: (e: ChangeEvent<HTMLSelectElement>) => void;
-  filterCategory?: string;
-  order?: string;
-  onConfirm: (data: Product) => void;
-  isLoading?: boolean;
-}
+const SortBar = memo(() => {
+  const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
 
-const SortBar = memo<SortBarProps>(
-  ({
-    onChangeSelect,
-    filterCategory,
-    order,
-    onConfirm,
-    isLoading,
-  }: SortBarProps) => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const navigate = useNavigate();
+  const { mutate: addProduct, isLoading: isLoadingAdd } = useAddProduct();
 
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    // Handle filter by category
-    const handleClickCategories = useCallback(
-      (value: string) => {
-        navigate(`${ROUTES.PRODUCT_SEARCH}?category=${value}`),
-          {
-            state: {
-              category: value,
-            },
-          };
-      },
-      [navigate],
-    );
+  const filterCategory = searchParams.get("category") || "";
+  const order = searchParams.get("order") || "";
 
-    const categories = [
-      {
-        id: WOMEN_CATEGORY.id,
-        img: WOMEN_CATEGORY.img,
-        alt: WOMEN_CATEGORY.alt,
-        label: WOMEN_CATEGORY.label,
-        action: handleClickCategories,
-        value: WOMEN_CATEGORY.value,
-      },
-      {
-        id: MEN_CATEGORY.id,
-        img: MEN_CATEGORY.img,
-        alt: MEN_CATEGORY.label,
-        label: MEN_CATEGORY.label,
-        action: handleClickCategories,
-        value: MEN_CATEGORY.value,
-      },
-    ];
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
 
-    return (
-      <Flex
-        p="10px"
-        mb="20px"
-        flexDir={{ xs: "column", md: "row" }}
-        justifyContent={{ xs: "center", md: "space-between" }}
-      >
-        <Flex justifyContent={{ xs: "center" }} alignItems="center">
-          {categories.map(({ id, action, label, value }: Category) => (
-            <Button
-              key={id}
-              variant="primary"
-              size={{ xs: "default", lg: "medium" }}
-              onClick={() => action(value)}
-              data-testid={id}
-              p={{ xs: "8px 30px", md: "10px 50px" }}
-              bg={filterCategory !== value ? "#3a3b3c" : "background.red"}
-              color={filterCategory !== value ? "text.default" : "text.default"}
-              width={{ xs: "100%" }}
-              border="none"
-            >
-              {label}
-            </Button>
-          ))}
+  // Handle filter by category
+  const handleClickCategories = useCallback(
+    (value: string) => {
+      navigate(`${ROUTES.PRODUCT_SEARCH}?category=${value}`),
+        {
+          state: {
+            category: value,
+          },
+        };
+    },
+    [navigate],
+  );
+
+  // Handle sort product
+  const handleChangeSelect = useCallback(
+    (e: ChangeEvent<HTMLSelectElement>) => {
+      const value = e.target.value;
+      searchParams.set("order", value);
+      setSearchParams(searchParams);
+    },
+    [searchParams, setSearchParams],
+  );
+
+  // Show error message when create fail
+  const handleError = useCallback((error: string) => {
+    toast({
+      title: error,
+      status: "error",
+      isClosable: true,
+    });
+  }, []);
+
+  // Show message when create success and close modal
+  const handleConfirmSuccess = useCallback(() => {
+    console.log("confirm", onClose);
+
+    onClose();
+    toast({
+      title: "Product created.",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  }, [onClose, toast]);
+
+  // Handle confirm add product
+  const handleConfirm = useCallback(
+    (data: Product) => {
+      addProduct(data, {
+        onSuccess: handleConfirmSuccess,
+        onError: (error) => handleError((error as AxiosError).message),
+      });
+    },
+    [addProduct],
+  );
+
+  const categories = [
+    {
+      id: WOMEN_CATEGORY.id,
+      img: WOMEN_CATEGORY.img,
+      alt: WOMEN_CATEGORY.alt,
+      label: WOMEN_CATEGORY.label,
+      action: handleClickCategories,
+      value: WOMEN_CATEGORY.value,
+    },
+    {
+      id: MEN_CATEGORY.id,
+      img: MEN_CATEGORY.img,
+      alt: MEN_CATEGORY.label,
+      label: MEN_CATEGORY.label,
+      action: handleClickCategories,
+      value: MEN_CATEGORY.value,
+    },
+  ];
+
+  return (
+    <Flex
+      p="10px"
+      mb="20px"
+      flexDir={{ xs: "column", md: "row" }}
+      justifyContent={{ xs: "center", md: "space-between" }}
+    >
+      <Flex justifyContent={{ xs: "center" }} alignItems="center">
+        {categories.map(({ id, action, label, value }: Category) => (
           <Button
-            ml="10px"
-            color="text.default"
-            px="15px"
-            onClick={onOpen}
-            data-testid="new-product"
+            key={id}
+            variant="primary"
+            size={{ xs: "default", lg: "medium" }}
+            onClick={() => action(value)}
+            data-testid={id}
+            p={{ xs: "8px 30px", md: "10px 50px" }}
+            bg={filterCategory !== value ? "#3a3b3c" : "background.red"}
+            color={filterCategory !== value ? "text.default" : "text.default"}
+            width={{ xs: "100%" }}
+            border="none"
           >
-            Add new product
+            {label}
           </Button>
-          <Suspense fallback={<LoadingIndicator />}>
-            {isOpen && (
-              <ModalForm
-                modalTitle="Product"
-                buttonLabel="Confirm"
-                onClose={onClose}
-                onConfirm={onConfirm}
-                isLoading={isLoading}
-                defaultValue={urlParams.get("category")!}
-              />
-            )}
-          </Suspense>
-        </Flex>
-        <Flex
-          alignItems="center"
-          textAlign="center"
-          justifyContent={{ xs: "center" }}
-          margin={{ xs: "10px", lg: "0" }}
+        ))}
+        <Button
+          ml="10px"
+          color="text.default"
+          px="15px"
+          onClick={onOpen}
+          data-testid="new-product"
         >
-          <Text
-            pr={{ xs: "5px", md: "10px" }}
-            w="100px"
-            color="#fff"
-            textAlign="left"
-          >
-            Sort by
-          </Text>
-          <Select
-            options={OPTION_SORT}
-            onChange={onChangeSelect}
-            value={order}
-          />
-        </Flex>
+          Add new product
+        </Button>
+        <Suspense fallback={<LoadingIndicator />}>
+          {isOpen && (
+            <ModalForm
+              modalTitle="Product"
+              buttonLabel="Confirm"
+              onClose={onClose}
+              onConfirm={handleConfirm}
+              isLoading={isLoadingAdd}
+              defaultValue={urlParams.get("category")!}
+            />
+          )}
+        </Suspense>
       </Flex>
-    );
-  },
-);
+      <Flex
+        alignItems="center"
+        textAlign="center"
+        justifyContent={{ xs: "center" }}
+        margin={{ xs: "10px", lg: "0" }}
+      >
+        <Text
+          pr={{ xs: "5px", md: "10px" }}
+          w="100px"
+          color="#fff"
+          textAlign="left"
+        >
+          Sort by
+        </Text>
+        <Select
+          options={OPTION_SORT}
+          onChange={handleChangeSelect}
+          value={order}
+        />
+      </Flex>
+    </Flex>
+  );
+});
 
 export default SortBar;
--- dress-shop/src/pages/ProductSearch/index.tsx
+++ dress-shop/src/pages/ProductSearch/index.tsx
@@ -1,7 +1,5 @@
-import { ChangeEvent, lazy, Suspense, useCallback } from "react";
-import { useSearchParams } from "react-router-dom";
-import { Container, useDisclosure, useToast } from "@chakra-ui/react";
-import { AxiosError } from "axios";
+import { lazy, Suspense } from "react";
+import { Container } from "@chakra-ui/react";
 
 // Components
 import { LoadingIndicator, SortBar } from "@/components";
@@ -9,69 +7,11 @@ const ProductListContainer = lazy(
   () => import("@/components/ProductListContainer"),
 );
 
-// Hooks
-import { useAddProduct } from "@/hooks";
-
-// Types
-import { Product } from "@/types";
-
 const ProductSearch = () => {
-  const toast = useToast();
-  const { onClose } = useDisclosure();
-
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const filterCategory = searchParams.get("category") || "";
-  const order = searchParams.get("order") || "";
-
-  const { mutate: addProduct, isLoading: isLoadingAdd } = useAddProduct();
-
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   console.log("urlParams", urlParams.get("order"));
 
-  // Handle sort product
-  const handleChangeSelect = useCallback(
-    (e: ChangeEvent<HTMLSelectElement>) => {
-      const value = e.target.value;
-      searchParams.set("order", value);
-      setSearchParams(searchParams);
-    },
-    [searchParams, setSearchParams],
-  );
-
-  // Show error message when create fail
-  const handleError = useCallback((error: string) => {
-    toast({
-      title: error,
-      status: "error",
-      isClosable: true,
-    });
-  }, []);
-
-  // Show message when create success and close modal
-  const handleConfirmSuccess = useCallback(() => {
-    console.log("confirm", onClose);
-
-    onClose();
-    toast({
-      title: "Product created.",
-      status: "success",
-      duration: 3000,
-      isClosable: true,
-    });
-  }, [onClose, toast]);
-
-  // Handle confirm add product
-  const handleConfirm = useCallback(
-    (data: Product) => {
-      addProduct(data, {
-        onSuccess: handleConfirmSuccess,
-        onError: (error) => handleError((error as AxiosError).message),
-      });
-    },
-    [addProduct],
-  );
   return (
     <>
       <Container
@@ -81,13 +21,7 @@ const ProductSearch = () => {
         p="0 15px"
         mt={{ lg: "80px" }}
       >
-        <SortBar
-          onChangeSelect={handleChangeSelect}
-          filterCategory={filterCategory}
-          order={order}
-          onConfirm={handleConfirm}
-          isLoading={isLoadingAdd}
-        />
+        <SortBar />
         <Suspense fallback={<LoadingIndicator />}>
           <ProductListContainer />
         </Suspense>
