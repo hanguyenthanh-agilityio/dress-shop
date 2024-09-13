72279a1117d84f869781bc573257578a14f92720
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-09-13 12:11:37 +0700

    fix: handle scroll to top

--- dress-shop/src/layouts/MainLayout/index.tsx
+++ dress-shop/src/layouts/MainLayout/index.tsx
@@ -1,13 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 // Components
 import { Header, Footer } from "@/components";
 import { Box } from "@chakra-ui/react";
+import { useEffect } from "react";
+
+export const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const MainLayout = () => {
   return (
     <Box bg="brand.900">
       <Header />
+      <ScrollToTop />
       <Outlet />
       <Footer />
     </Box>
